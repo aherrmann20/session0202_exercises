1d12a8c219f42a589075fa1eea90bb7070f618fe
aherrmann20 <aherrmann@college.harvard.edu>
2015-06-19 10:11:25 +0200

    Worked on phonebook, completed code for part one of 4A q5

--- AnnelieHerrmann-aherrmann20/d4/finding_things.rb
+++ AnnelieHerrmann-aherrmann20/d4/finding_things.rb
@@ -1,10 +1,16 @@
 def index_of(string, letter)
-	string_array = [string.split('')]
+	string_array = string.chars.to_a
+
+	index=0
+
 	string_array.each do |element|
-		if element == letter
-			element
+		if element != letter
+			index = index + 1
+		else
+			break
 		end
 	end
+	index
 end
 
 puts index_of("abcde", "d")
--- AnnelieHerrmann-aherrmann20/d4/phonebook/app.rb
+++ AnnelieHerrmann-aherrmann20/d4/phonebook/app.rb
@@ -1,9 +1,19 @@
 get '/' do
-	
+	"Welcome!"
 end
 
-get '/contacts'
+get '/contacts' do
+	@contacts = ["Khangwelo Malotsha", "Jessie Osterman", "Christopher Cahill"]
+	erb :contacts
 
 end
 
-get '/contacts/:contact'
+get '/contacts/:contact' do |contact|
+	contacts = {"Khangwelo_Malotsha" => "0829294183", "Jessie Osterman" => "0712284058", "Christopher Cahill" => "0716444576"}
+	contact = params["contact"]
+	number = contacts[contact]
+
+	@contact_name = params["contact"]
+	@number = contacts[@contact_name]
+	erb :contact
+end
