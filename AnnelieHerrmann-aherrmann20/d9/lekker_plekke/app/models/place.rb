2ac6477601b51332a0556838ceb814cca341dfc3
aherrmann20 <aherrmann@college.harvard.edu>
2015-06-25 21:14:40 +0200

    Got comments working

--- AnnelieHerrmann-aherrmann20/d9/lekker_plekke/app/models/place.rb
+++ AnnelieHerrmann-aherrmann20/d9/lekker_plekke/app/models/place.rb
@@ -1,2 +1,4 @@
 class Place < ActiveRecord::Base
+	has_many :comments
+	validates :title, presence: true, length: { minimum: 5 }
 end
--- AnnelieHerrmann-aherrmann20/d9/lekker_plekke/db/schema.rb
+++ AnnelieHerrmann-aherrmann20/d9/lekker_plekke/db/schema.rb
@@ -11,7 +11,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150624171442) do
+ActiveRecord::Schema.define(version: 20150625191031) do
+
+  create_table "comments", force: :cascade do |t|
+    t.string   "commenter"
+    t.text     "body"
+    t.integer  "article_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.integer  "place_id"
+  end
+
+  add_index "comments", ["article_id"], name: "index_comments_on_article_id"
 
   create_table "places", force: :cascade do |t|
     t.string   "name"
