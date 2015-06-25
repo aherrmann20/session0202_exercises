0542902265fc7a318cf600e6f5cbef901cb4127b
aherrmann20 <aherrmann@college.harvard.edu>
2015-06-25 15:30:57 +0200

    Added validations

--- AnnelieHerrmann-aherrmann20/d8/cheetah_watch/app/controllers/animals_controller.rb
+++ AnnelieHerrmann-aherrmann20/d8/cheetah_watch/app/controllers/animals_controller.rb
@@ -6,4 +6,47 @@ class AnimalsController < ApplicationController
 	def show
 		@animal = Animal.find params[:id]
 	end
+
+	def new
+		@animal = Animal.new
+	end
+
+	def edit
+		@animal = Animal.find(params[:id])
+	end
+
+	def create
+		@animal = Animal.new(animal_params)
+
+		if @animal.save
+			redirect_to @animal
+		else
+			render 'new'
+		end
+	end
+
+	def update
+		#don't need an instance variable because we don't have a view that needs to use it
+		#I used one here anyway because what's the harm?
+		@animal = Animal.find(params[:id])
+
+		if @animal.update(animal_params)
+			redirect_to @animal
+		else
+			#does not CALL the edit method
+			render 'edit'
+		end
+	end
+
+	def destroy
+		@animal = Animal.find(params[:id])
+		@animal.destroy
+
+		redirect_to animals_path
+	end
+
+	private
+	def animal_params
+		params.require(:animal).permit(:name, :region, :life_expectancy, :conservation_status)
+	end
 end
--- AnnelieHerrmann-aherrmann20/d8/cheetah_watch/app/models/animal.rb
+++ AnnelieHerrmann-aherrmann20/d8/cheetah_watch/app/models/animal.rb
@@ -1,2 +1,6 @@
 class Animal < ActiveRecord::Base
+	#says we HAVE to have something in the "name" and "region" columns for the new animal to save
+	validates :name, :region, presence: true
+	validates :life_expectancy, numericality: {only_integer: true, greater_than: 0}
+	validates :name, uniqueness: {case_sensitive: false}
 end
